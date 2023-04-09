4b16d1427e1fb99f413981efacc6488c375e406a
Ítalo Miranda Fontenele <113559117+Ital023@users.noreply.github.com>
2023-04-09 14:02:02 -0300

    Adicionando a funcionalidade Game Over

--- scripts/validacao.js
+++ scripts/validacao.js
@@ -2,9 +2,21 @@ function verificaNumeroValido(chute){
     //transforma em numero int
     const numero = +chute;
 
+    
     if(verificaNumero(numero) == true){
-        idChute.innerHTML += `<div>O valor está invalido</div>` 
-        return
+        if(chute == "game over"){
+            document.body.innerHTML = 
+            `
+            <main class="game-over">
+            <h2 class="titulo">Game Over</h2>
+            <button id="jogar-novamente" class="btn-novamente">Jogar Novamente</button>
+            </main>
+            ` 
+            return
+        }else{
+            idChute.innerHTML += `<div>O valor está invalido</div>` 
+            return
+        }
     }
     if(numero > maiorNumero || numero < menorNumero){    
         idChute.innerHTML += `<div>Valor inválido: Fale um número entre ${menorNumero} e ${maiorNumero}</div>`
@@ -20,7 +32,8 @@ function verificaNumeroValido(chute){
             </main>
         `
         return 
-    }else if(numero < numeroGerado){
+    }
+    else if(numero < numeroGerado){
         idChute.innerHTML += `<p>O numero secreto é maior <i class="fa-solid fa-up-long"></i></p>`
     }else{
         idChute.innerHTML += `<p>O numero secreto é menor <i class="fa-solid fa-down-long"></i></p>`
