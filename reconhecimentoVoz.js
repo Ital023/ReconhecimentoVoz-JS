51e5c52fb46437c634d1efa9d7119b685d1e0e44
Ítalo Miranda Fontenele <113559117+Ital023@users.noreply.github.com>
2023-04-08 21:07:32 -0300

    Adicionando o leitor de voz e exibindo na tela

---
+++ reconhecimentoVoz.js
@@ -0,0 +1,20 @@
+window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
+
+const idChute = document.querySelector("#chute");
+const recognition = new SpeechRecognition();
+recognition.lang = "pt-Br";
+recognition.start();
+
+recognition.addEventListener("result",onSpeak);
+
+function onSpeak(evento){
+   chute = evento.results[0][0].transcript;
+   exibeChuteNaTela(chute);
+}
+
+function exibeChuteNaTela(chute) {
+    idChute.innerHTML = `
+        <div>Você disse</div>
+        <span class="box">${chute}</span>
+     `
+}
